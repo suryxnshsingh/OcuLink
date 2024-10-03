e622ff8811f8a7da266a444a0b0cdc1b3963884d
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2024-10-04 00:14:30 +0530

    nav

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from '@/components/mobileNav'
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
 import React, { ReactNode } from 'react'
@@ -5,7 +6,8 @@ import React, { ReactNode } from 'react'
 const HomeLayout = ({children}:{ children: ReactNode }) => {
   return (
     <main className='relative'>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <MobileNav/>
       <div className='flex'>
         <Sidebar/>
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=''>
+        Navbar
+    
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar 
